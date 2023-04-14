eb84264b44ce3e09538d647a3013690e3a93914d
Silas Mächler <silas.maechler@ypsomed.com>
2023-04-14 13:27:00 +0200

    aufgabe 3

--- 02-conditions/solution.py
+++ 02-conditions/solution.py
@@ -1,9 +1,6 @@
 wohnort = "Aarberg"
 wohnort2 = "Bern"
 
-print("BASIC")
-print("------------------")
-
 if wohnort == "Aarberg":
     print("Ich wohne in " + wohnort)
 else:
@@ -14,28 +11,3 @@ if wohnort2 == "Aarberg":
     print("Ich wohne in: " + wohnort2)
 else:
     print("Ich wohne nicht in:  " + wohnort2)
-
-print(" ")
-print("EXTENDED")
-print("------------------")
-
-# Erweitert:
-orte = [
-    "Aarberg",
-    "Bern",
-    "Zürich",
-    "Basel",
-    "Luzern",
-    "St. Gallen",
-    "Genf",
-    "Lausanne",
-    "Winterthur",
-    "Lugano",
-]
-wohnort = "Aarberg"
-
-for ort in orte:
-    if wohnort == ort:
-        print("Ich wohne in " + ort)
-    else:
-        print("Ich wohne nicht in " + ort)
